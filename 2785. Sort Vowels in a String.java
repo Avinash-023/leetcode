bed628eebb4a384dd71720f5160c92679c0ebbca
Avinash-023 <231001023@rajalakshmi.edu.in>
2025-09-11 18:33:41 +0530

    255 ms Beats 47.71%

---
+++ 2785. Sort Vowels in a String.java
@@ -0,0 +1,22 @@
+class Solution(object):
+    def sortVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        l=['a','e','i','o','u','A','E','I','O','U']
+        ans=[]
+        for i in s:
+            if i in l:
+                ans.append(i)
+        ans.sort()
+        j=0
+        res=[]
+        for i in s:
+            if i in l:
+                res.append(ans[j])
+                j+=1
+            else:
+                res.append(i)
+        return "".join(res)
+        
