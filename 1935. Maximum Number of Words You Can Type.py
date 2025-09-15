826a2f98662e0f6a5557a84861d1fc81c11f923b
Avinash-023 <231001023@rajalakshmi.edu.in>
2025-09-14 20:46:32 -0700

    1 ms Beats 48.12%

---
+++ 1935. Maximum Number of Words You Can Type.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        l=text.split()
+        count=0
+        for i in l:
+            s=True
+            for j in i:
+                if j in brokenLetters:
+                    s=False
+            if s==True:
+                count+=1
+        return count
+                
+        
